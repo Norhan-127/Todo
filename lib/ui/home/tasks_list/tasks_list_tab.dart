5642cc5edcf7bbf7c0991e7fb351d969c37cb715
Norhan-127 <norhanhamza127@gmail.com>
2023-04-15 11:21:16 +0200

    adding calender time line

--- lib/ui/home/tasks_list/tasks_list_tab.dart
+++ lib/ui/home/tasks_list/tasks_list_tab.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:todo/ui/home/tasks_list/calender.dart';
 class TasksListTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-    );
+    return Column(children: [
+      Calender(),
+    ],);
   }
 }
